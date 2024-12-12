c9d07a6b6e1a00622b97ba55e74727e68c478218
Saliha Kotan <salihakotan77@gmail.com>
2024-12-12 11:41:23 +0300

    UI pages added with livewire, routes defined

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,39 @@
 <?php
 
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\OrderDetailPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class)->name('home');
+Route::get('/categories', CategoriesPage::class)->name('categories');
+Route::get('/products', ProductsPage::class)->name('products');
+Route::get('/cart', CartPage::class)->name('cart');
+Route::get('/products/{product}', ProductDetailPage::class);
+
+
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', action: MyOrdersPage::class);
+Route::get('/my-orders/{order}', OrderDetailPage::class);
+
+
+
+Route::get('/login', LoginPage::class);
+Route::get('/register', RegisterPage::class);
+Route::get('/forgot', ForgotPasswordPage::class);
+Route::get('/reset', ResetPasswordPage::class);
+
+
+Route::get('/success', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
