b47d3cc2b67372bec10587e0824f9bad3604f265
Saliha Kotan <salihakotan77@gmail.com>
2024-12-13 02:17:11 +0300

    my orders page content

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('My Orders - UHDWonders')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $my_orders = Order::where('user_id',auth()->guard()->id())
+        ->latest()->paginate(5);
+        return view('livewire.my-orders-page',[
+            'orders'=>$my_orders,
+        ]);
     }
 }
